d93bb3410783aa097f21ece89b5219c44fc0b47e
Lucas Rene <104390822+LRenne@users.noreply.github.com>
2023-03-07 18:43:41 -0300

    Add files via Upload.

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
+
+class SolarSystem extends React.Component {
+  render() {
+    const Planets = planets.map(({ name, image }) => (
+      <PlanetCard key={ name } planetName={ name } planetImage={ image } />
+    ));
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        {Planets}
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
